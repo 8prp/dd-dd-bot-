32646712d5b2959e47f88409db1283e3673fdcff
8pr8 <40867383+8pr8@users.noreply.github.com>
2018-07-15 00:13:20 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+
+كل الاكواد الي تبيها و عندنا مانع نشر و مانع جحفله 
+الي يخطر ببالك 
+افضل سيرفر برمجه عربي . 
+ تعال وصير مبرمج   :kissing_heart:, !
+و آتحدآك مآ يعجبك :wink:, !
+رآبط آلسيرفر:arrow_down_small::arrow_down_small::arrow_down_small:
+( Codes Official :revolving_hearts: )
+[ https://discord.gg/6MedSh3 ] [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD.login("NDYyOTc4MzAwNDg3MTM5MzMw.DhptpA.U4o-7_MihPrQliwqi76ZR4deVnw");
